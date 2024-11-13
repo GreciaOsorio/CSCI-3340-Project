c8216f92b8dd0ef8d9eeb6602800087dc8dafa5e
Grecia Osorio <you@example.com>
2024-11-12 18:05:27 -0600

    sign up page version: role decision

--- fall2024/test/testApp/views.py
+++ fall2024/test/testApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from .forms import CreateUserForm
+#from .filters import OderFilter
 
 # Create your views here.
 def home(request):
@@ -10,3 +16,15 @@ def home(request):
 
 #def logout_user(request):
 #    pass
+
+def signup(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid:
+            form.save()
+
+
+
+    context= {'form': form}
+    return render (request, 'signup.html', context)
