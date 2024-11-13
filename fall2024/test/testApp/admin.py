22db99a9201a2b75b17aae84004c1e4fa15df66e
Kailey Chavez <kachavez165@gmail.com>
2024-11-12 21:52:40 -0600

    Create models + register them on Django admin site

--- fall2024/test/testApp/admin.py
+++ fall2024/test/testApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile, Task, Project
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Task)
+admin.site.register(Project)
