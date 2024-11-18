73e01e548005992492d29c709268810b6979c8a3
Kailey Chavez <kachavez165@gmail.com>
2024-11-17 18:45:05 -0600

    Managers can create/update projects & their tasks

--- fall2024/test/testApp/urls.py
+++ fall2024/test/testApp/urls.py
@@ -17,9 +17,16 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
-    #path('dashboard/', views.dashboard, name='dashboard'),  # New dashboard URL
+    path('login/', views.login_view, name='login'),
+    # path('dashboard/', views.prepare_dashboard_context, name='dashboard'),
     # path('managerDash/', views.managerDashboard, name='managerDash'), 
     # path('teammateDash/', views.teammateDashboard, name = 'teammateDash')
+    path('projects/', views.project_list_view, name='project_list'),
+    path('projects/<int:id>/', views.project_detail_view, name='project_detail'),
+    path('projects/manager/create/', views.create_project_view, name='create_project'),
+    path('projects/manager/<int:id>/update/', views.update_project_view, name='update_project'),
+    path('projects/manager/<int:id>/tasks/create/', views.create_task_view, name='create_task'),
+    path('projects/manager/<int:id>/tasks/<int:t_id>/update/', views.update_task_view, name='update_task'),
+    # This is pending: path('projects/teammate/<int:id>/tasks/<int:t_id>/update/', views.update_task_status_view, name='update_task_status')
 ]
--- fall2024/test/testApp/views.py
+++ fall2024/test/testApp/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import Group
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from .forms import SignUpForm, LoginForm
-from .models import UserProfile, Project
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect #check why we have to keep using these if they are supposed to be automatic 
-
+from django.utils import timezone
+from .forms import SignUpForm, LoginForm
+from .models import UserProfile, Project, Task
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
-
 
 @csrf_protect
 def signup_view(request):
@@ -41,7 +42,7 @@ def signup_view(request):
     context = {'form': form}
     return render(request, 'signup.html', context)
 
-#helper
+# Helper for login_view
 def prepare_dashboard_context(user, is_manager):
     if is_manager:
         projects = Project.objects.filter(p_manager=user)
@@ -60,7 +61,7 @@ def prepare_dashboard_context(user, is_manager):
     return context, template_name
 
 @csrf_protect
-@ensure_csrf_cookie #ensures token is available for form, helps if youre doing a lot of testing 
+@ensure_csrf_cookie # Ensures token is available for form; helps if you're doing a lot of testing. 
 def login_view(request):
     if request.method == 'POST': 
         form = LoginForm(request.POST)
@@ -70,21 +71,162 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             
             if user is not None:
-                # This messes with your Django admin login (logs you out), but why?
-                #call function here for creating user context 
+                # Call function here for creating user context.
                 login(request, user)
                 messages.success(request, f'Successful login for: {username}.')
-                
-                # Determine user type and prepare dashboard
+                # Determine user type and prepare their dashboard.
                 user_type = user.userprofile.user_type
-                is_manager = user_type == 'manager' #checks if user is manager otherwise itll 'else' to teammate 
+                is_manager = user_type == 'manager' # Checks if user is a manager. Otherwise, it'll 'else' to teammate. 
                 context, template_name = prepare_dashboard_context(request.user, is_manager)
-                            # If our user w/ entered info exists in our database...
+                # If our user w/ entered info exists in our database...
                 return render(request, template_name, context)
             else: # Same here, our login success message shows in Django admin (make sure to refresh page).
-
                 messages.error(request, 'Invalid username or password.')
     else:
         form = LoginForm()
 
     return render(request, 'login.html', {'form': form})
+
+
+# All Cubby users can view their project lists and specific project details.
+@login_required
+def project_list_view(request):
+    if request.user.userprofile.user_type == 'manager':
+        projects = Project.objects.filter(p_manager=request.user)
+    else:
+        projects = Project.objects.filter(p_members=request.user)
+    
+    context = {
+        'projects': projects,
+        'user_type': request.user.userprofile.user_type
+    }
+    return render(request, 'projectList.html', context)
+
+@login_required
+def project_detail_view(request, id):
+    if request.user.userprofile.user_type == 'manager':
+        project = get_object_or_404(Project, id=id, p_manager=request.user)
+    else:
+        project = get_object_or_404(Project, id=id, p_members=request.user)
+    
+    tasks = project.tasks.all()
+    context = {
+        'project': project,
+        'tasks': tasks,
+        'user_type': request.user.userprofile.user_type
+    }
+    return render(request, 'projectDetail.html', context)
+
+
+# Unique to managers: can create, update, and delete projects/tasks.
+@login_required
+def create_project_view(request):
+    if request.user.userprofile.user_type != 'manager':
+        raise PermissionDenied
+    
+    # Get all users who are teammates (in theory, should work, but it's not).
+    available_teammates = User.objects.filter(
+        userprofile__user_type='teammate'
+    )
+    
+    if request.method == 'POST':
+        project = Project.objects.create(
+            p_name=request.POST['name'],
+            p_description=request.POST['description'],
+            p_due_date=request.POST['due_date'],
+            p_manager=request.user
+        )
+        project.p_members.set(request.POST.getlist('members'))
+
+        # Takes us to view the newly-created project info from the "Project Detail" page.
+        return redirect('project_detail', id=project.id)
+    
+    context = {
+        'available_teammates': available_teammates
+    }
+    return render(request, 'createProject.html', context)
+
+# If a project member is removed, their tasks should disappear (need to add this).
+@login_required
+def update_project_view(request, id):
+    if request.user.userprofile.user_type != 'manager':
+        raise PermissionDenied
+    
+    project = get_object_or_404(Project, id=id, p_manager=request.user)
+    
+    if request.method == 'POST':
+        project.p_name = request.POST['name']
+        project.p_description = request.POST['description']
+        project.p_due_date = request.POST['due_date']
+        # Debating if the two following lines are necessary...
+        project.p_modified_date = timezone.now()
+        project.save()
+        project.p_members.set(request.POST.getlist('members'))
+
+        return redirect('project_detail', id=project.id)
+    
+    context = {
+        'project': project,
+        'members': User.objects.all()
+    }
+    return render(request, 'updateProject.html', context)
+
+@login_required
+def create_task_view(request, id):
+    if request.user.userprofile.user_type != 'manager':
+        raise PermissionDenied
+    
+    project = get_object_or_404(Project, id=id, p_manager=request.user)
+    # Show all the users are currently working on the task's related project.
+    project_members = project.p_members.all()
+
+    if request.method == 'POST':
+        task = Task.objects.create(
+            project=project,
+            t_name=request.POST['name'],
+            t_description=request.POST['description'],
+            t_due_date=request.POST['due_date'],
+            t_manager=request.user
+        )
+        task.t_assignees.set(request.POST.getlist('assignees'))
+
+        return redirect('project_detail', id=project.id)
+    
+    context = {
+        'project': project,
+        'project_members': project_members
+    }
+    return render(request, 'createTask.html', context)
+
+@login_required
+def update_task_view(request, id, t_id):
+    if request.user.userprofile.user_type != 'manager':
+        raise PermissionDenied
+    
+    project = get_object_or_404(Project, id=id, p_manager=request.user)
+    task = get_object_or_404(Task, id=t_id, project=project)
+    
+    if request.method == 'POST':
+        task.t_name = request.POST['name']
+        task.t_description = request.POST['description']
+        task.t_due_date = request.POST['due_date']
+        # Check if these 2 lines need to be left in.
+        task.t_modified_date = timezone.now()
+        task.save()
+        task.t_assignees.set(request.POST.getlist('assignees'))
+        
+        return redirect('project_detail', id=project.id)
+    
+    context = {
+        'project': project,
+        'task': task,
+        'assignees': User.objects.all()
+    }
+    return render(request, 'updateTask.html', context)
+
+# Need to add views to delete projects AND tasks.
+
+
+# Unique to teammates (a.k.a project members, task assignees): can update the status of their assigned task(s).
+# what needs to be done here: update_task_status_view (for project members/assignees)
+# add url for it, too
